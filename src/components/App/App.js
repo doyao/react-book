7b5c3466ccee855044aaec19519bcd6684104b0c
doyao <495775514@qq.com>
2021-03-19 18:06:24 +0800

    2021-3-19

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import '../../styles/App/index.less';
+import {Button} from 'antd';
+function App() {
+  return (
+    <div className="App">
+     <Button type="primary">123</Button>
+      我是less
+    </div>
+  );
+}
+
+export default App;
